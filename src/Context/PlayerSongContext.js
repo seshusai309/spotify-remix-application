5eb35e66f591240388cefeb99e0f39fca0d22ba4
seshu <saiseshar@gmail.com>
2025-04-16 04:52:36 +0000

    changed context structure to be same as in provider context value

--- src/Context/PlayerSongContext.js
+++ src/Context/PlayerSongContext.js
@@ -3,6 +3,8 @@ import React from 'react'
 const PlayerSongContext = React.createContext({
   playingTrackUrl: '',
   onClickAddTrack: () => {},
+  isPlaying: false,
+  changeState: () => {},
 })
 
 export default PlayerSongContext
